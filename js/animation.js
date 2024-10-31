bc04e1f9aa3cbd136ec4f7baf343885722f1ecbe
driel4m <adrielcastor7@gmail.com>
2024-10-30 23:25:23 -0300

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,19 @@
+//window.addEventListener("load", () => {
+//    document.body.classList.add("loaded");
+//});
+
+ 
+//const faders = document.querySelectorAll('.fade-in');
+
+//const appearOnScroll = () => {
+//    faders.forEach(fader => {
+//        const rect = fader.getBoundingClientRect();
+//        if (rect.top <= window.innerHeight - 100) {
+//            fader.classList.add('visible');
+//       }
+//    });
+//};
+
+
+//window.addEventListener('scroll', appearOnScroll);
+//window.addEventListener('load', appearOnScroll);
