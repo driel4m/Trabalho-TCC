fc228ca7b1aac6a7572f20f54880e30b2b005776
Anderson Freitas <freitas.dev@proton.me>
2024-10-31 00:32:50 -0300

    :fire: Fix CSS. CHAMA :moyai:

--- js/animation.js
+++ js/animation.js
@@ -1,19 +1,19 @@
-//window.addEventListener("load", () => {
-//    document.body.classList.add("loaded");
-//});
+window.addEventListener("load", () => {
+   document.body.classList.add("loaded");
+});
 
  
-//const faders = document.querySelectorAll('.fade-in');
+const faders = document.querySelectorAll('.fade-in');
 
-//const appearOnScroll = () => {
-//    faders.forEach(fader => {
-//        const rect = fader.getBoundingClientRect();
-//        if (rect.top <= window.innerHeight - 100) {
-//            fader.classList.add('visible');
-//       }
-//    });
-//};
+const appearOnScroll = () => {
+   faders.forEach(fader => {
+       const rect = fader.getBoundingClientRect();
+       if (rect.top <= window.innerHeight - 100) {
+           fader.classList.add('visible');
+      }
+   });
+};
 
 
-//window.addEventListener('scroll', appearOnScroll);
-//window.addEventListener('load', appearOnScroll);
+window.addEventListener('scroll', appearOnScroll);
+window.addEventListener('load', appearOnScroll);
